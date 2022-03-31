d895df279aeee12430a6d074c84722bbdcec861a
ADIOP55550 <ak117805@stud.ur.edu.pl>
2022-03-31 17:49:15 +0200

    GraphStream added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,17 @@
 module pl.edu.ur.pnes {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.swing;
+    requires javafx.graphics;
     requires tiwulfx.dock;
     requires org.jfxtras.styles.jmetro;
+    requires gs.core;
+    requires gs.ui.javafx;
 
 
     opens pl.edu.ur.pnes to javafx.fxml;
     exports pl.edu.ur.pnes;
     exports pl.edu.ur.pnes.panels;
+
+//    exports com.sun.javafx.logging;
 }
--- src/main/java/pl/edu/ur/pnes/panels/CenterPanel.java
+++ src/main/java/pl/edu/ur/pnes/panels/CenterPanel.java
@@ -1,7 +1,14 @@
 package pl.edu.ur.pnes.panels;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.fx_viewer.FxViewer;
+import org.graphstream.ui.view.View;
+import org.graphstream.ui.view.Viewer;
 
 public class CenterPanel extends CustomPanel {
 
@@ -11,5 +18,31 @@ public class CenterPanel extends CustomPanel {
     public HBox centerToolbarLeft;
     @FXML
     public HBox centerToolbar;
+    @FXML
+    public Pane graphPane;
 
+
+    public void initialize() {
+        System.setProperty("org.graphstream.ui", "javafx");
+
+        Graph graph = new SingleGraph("Tutorial 1");
+
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("AB", "A", "B");
+        graph.addEdge("BC", "B", "C");
+        graph.addEdge("CA", "C", "A");
+
+
+//        graph.display();
+        Viewer viewer = new FxViewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
+
+
+// ...
+        View view = viewer.addDefaultView(false);   // false indicates "no JFrame".
+//        view.getCamera().setViewPercent(0.5);
+// ...
+        graphPane.getChildren().add((Node) view);
+    }
 }
