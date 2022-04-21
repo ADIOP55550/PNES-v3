8411a966ac80598cc423a2cf42741e3bed192ea3
jm117818 <jmazurek.kontakt@gmail.com>
2022-04-21 22:06:11 +0200

    Icon Font/Add Font Custom Control/

--- src/main/java/pl/edu/ur/pnes/MainApp.java
+++ src/main/java/pl/edu/ur/pnes/MainApp.java
@@ -1,16 +1,20 @@
 package pl.edu.ur.pnes;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import jfxtras.styles.jmetro.JMetro;
 import jfxtras.styles.jmetro.Style;
+import pl.edu.ur.pnes.petriNet.Place;
 import pl.edu.ur.pnes.ui.PanelManager;
 import pl.edu.ur.pnes.ui.panels.CenterPanel;
 import pl.edu.ur.pnes.ui.panels.ProjectTreePanel;
 import pl.edu.ur.pnes.ui.panels.PropertiesPanel;
 
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -28,6 +32,12 @@ public class MainApp extends Application {
 
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320 * 3, 240 * 3);
+
+//        scene.getStylesheets().add("/home/jakub/Desktop/PNES-v3/src/main/resources/css/font.css");
+        String cssy = Objects.requireNonNull(getClass().getResource("/css/font.css")).toExternalForm();
+        scene.getStylesheets().add(cssy);
+
+
         stage.setTitle("PNES");
         stage.setMaximized(true);
         mainJMetro.setScene(scene);
@@ -35,9 +45,21 @@ public class MainApp extends Application {
         stage.show();
         mainStage = stage;
 
+//        mainJMetro.reApplyTheme();
+//        mainJMetro.getOverridingStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/font.css")).toExternalForm());
+//        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/font.css")).toExternalForm());
+
+        mainJMetro.styleProperty().addListener(observable -> {
+            scene.getStylesheets().remove(cssy);
+            Platform.runLater(() -> {
+                scene.getStylesheets().add(cssy);
+            });
+        });
+
         mainController = fxmlLoader.getController();
 
         MainApp.centerPanel = new CenterPanel();
+
 
         PanelManager.addCenterPanel(centerPanel);
         PanelManager.addRightPanel(new PropertiesPanel());
